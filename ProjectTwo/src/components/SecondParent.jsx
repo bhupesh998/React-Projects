fe0457d87761d356a017fcb2f57128cfffcbda66
bhupesh998 <bhupeshchoudhary998@gmail.com>
2024-09-06 22:18:10 +0530

    useCallback with function

--- ProjectTwo/src/components/SecondParent.jsx
+++ ProjectTwo/src/components/SecondParent.jsx
@@ -1,15 +1,20 @@
 import React, { useCallback } from 'react'
 import ThirdParent from './ThirdParent.jsx';
 
-const SecondParent = (props) => {
-    let {count}= props
+const SecondParent = () => {
+   
+    const myFunctionSecond=useCallback(()=>{
+        console.log("Second Function");
+        
+    },[])
 
     // this use call back is helping us because third component is slow but it uses a memo so if props don't change third component will not be called
     // so our outer component is unaffected by third component slowness now by use of useCallback as on rerender third props are not changing and i.e why not affecting outer count
     const handleChange = useCallback(()=>{
-        console.log("change in second parent", count);
+        console.log("change in second parent");
+        myFunctionSecond()
         
-    }, [count])
+    }, [myFunctionSecond])
     // useCallback also take reactive variables , reactive variables are variable that change value on rerender
     // React says whatever reactive variable you are using inside callback function you have to pass it in dependency array
     // Assume we don't pass count in useCallback then on first render whatever the count value will be there will remain same 
