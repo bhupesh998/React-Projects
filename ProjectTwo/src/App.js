01526e5af9a9e53603ebd1060213ae949179495c
bhupesh998 <bhupeshchoudhary998@gmail.com>
2024-09-07 08:43:01 +0530

    Memo api , Object.is and Memo with UseMemo

--- ProjectTwo/src/App.js
+++ ProjectTwo/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 import TimerComponent from './components/TimerComponent.jsx'
 import ButtonWithTooltip from './components/ButtonWithTooltip.jsx';
 import InputComponent from './components/InputComponent.jsx';
@@ -9,11 +9,18 @@ import PrintTable from './components/PrintTable.jsx';
 export default function App() {
   const [counter, setCounter]= useState(0)
   const [counterTwo, setCounterTwo]= useState(0)
+  //const [myObj = setMyObj] = useState({channel: "Ted"})
+  const myObj = useMemo(()=>{channel: "Ted"}, []) 
+  const arr= useMemo(()=>["test", "array"], [])
+  const val =10
   return <>
   
     Counter1: {counter} <br/> <button onClick={()=>setCounter((prev)=>++prev)}>Count1</button>
     Counter2: {counterTwo}<br/> <button onClick={()=>setCounterTwo((prev)=>++prev)}>Count2</button>
 
-    <PrintTable num={counter}/>
+    {/* <PrintTable num={counter} obj={myObj}/> */}
+    {/* <PrintTable num={counter}  val={val}/> */}
+    {/* <PrintTable num={counter}  arr={arr}/> */}
+    <PrintTable num={counter} obj={myObj}  arr={arr} val={val}/>
   </>
 }
