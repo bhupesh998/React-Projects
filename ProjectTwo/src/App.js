728f048a9899aef8a714eaf4e95cec03ccfc4a35
bhupesh998 <bhupeshchoudhary998@gmail.com>
2024-09-07 15:48:53 +0530

    react redux toolkit

--- ProjectTwo/src/App.js
+++ ProjectTwo/src/App.js
@@ -1,14 +1,17 @@
 import React, {  useState} from 'react'
-import { createPortal } from 'react-dom'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incByAmount } from './components/counterSlice'
 
 export default function App() {
- const [showModal, toggleModal] = useState(false)
+  
+  const count = useSelector((state) => state.counterKey.counterValue)
+  const dispatch = useDispatch()
+
   return <>
-   <h1 >Hey i am root</h1>
-   <button onClick={()=>toggleModal((prev)=>!prev)}>Toggle Modal</button>
-   {showModal && <div>This is Modal Content</div>}
-   {showModal && createPortal(<div>This is Modal Content</div>, document.body)}
+    <h1>{count}</h1>
+    <button onClick={()=>dispatch(increment())}>Increase</button>
+    <button onClick={()=>dispatch(decrement())}>Decrase</button>
+    <button onClick={()=>dispatch(incByAmount(6))}>Increase by N</button>
+  
   </>
 }
-
--- ProjectTwo/src/index.js
+++ ProjectTwo/src/index.js
@@ -4,10 +4,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./styles.css"
+import { storeVal } from './components/Store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 
 
-root.render(<App/>)
+root.render(<Provider store={storeVal}>
+    <App />
+  </Provider>
+)
 
