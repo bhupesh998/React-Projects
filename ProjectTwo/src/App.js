597158005319e8922c5f93937fe486d68c7dbbf1
bhupesh998 <bhupeshchoudhary998@gmail.com>
2024-09-09 21:50:43 +0530

    basic data get api

--- ProjectTwo/src/App.js
+++ ProjectTwo/src/App.js
@@ -1,12 +1,28 @@
-import React , {useState} from 'react'
-import HomePage from './components/HomePage.jsx'
-import ChatPage from './components/ChatPage.jsx'
+import React , {useEffect, useState} from 'react'
+
 
 
 export default function App(){
+
+  const [photos, setPhotos] = useState()
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const response = await fetch(`https://jsonplaceholder.typicode.com/photos`)
+      const data =await response.json()
+      console.log(data);
+      setPhotos(data)
+    }
+    fetchData()
+  }, [])
+
   return <>
-  <HomePage /> <br/>
-  <ChatPage />
+  <h1>PHOTOS API</h1>
+
+  { photos && photos?.length > 0 ?  photos.map((pic)=> <img key={pic?.id} loading='lazy' height={100} width={100} src={pic?.url} alt={pic?.title} />): <span>Loading Images</span> }  
+
+ 
+
   
   </>
 }
