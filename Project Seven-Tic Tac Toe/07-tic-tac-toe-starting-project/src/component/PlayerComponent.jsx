cd1c8157912378d5b364cb527b9e8aa191a848ad
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-06 23:56:16 +0530

    two way binding - listening to change on input and feeding the updated value back to input

--- Project Seven-Tic Tac Toe/07-tic-tac-toe-starting-project/src/component/PlayerComponent.jsx
+++ Project Seven-Tic Tac Toe/07-tic-tac-toe-starting-project/src/component/PlayerComponent.jsx
@@ -3,22 +3,27 @@ import React, { useState} from 'react'
 const PlayerComponent = ({name , symbol}) => {
 
  const [isEditing , setIsEditing] = useState(false)
+ const [playerName, setPlayerName] = useState(name)
  const handleClick =()=>{
         setIsEditing(prevState=> !prevState) //this way is used so react always use latest state value while updating the state
 
  }
 
- let playerName =<span className="player-name">{name}</span> 
+ const handleChange =(event)=>{
+    setPlayerName(event.target.value)
+ }
+
+ let playerNameTag =<span className="player-name">{playerName}</span> 
  let btnCaption = 'Edit'
  if(isEditing){
-    playerName = <input type='text' required value={name}/> 
+    playerNameTag = <input type='text' required value={playerName} onChange={handleChange}/> //instead of value={name} you can use defaultValue because when trying to edit value the value will not change because of value property as it will override the value that you are trying to edit or other way using state
     btnCaption = 'Save'
  }
 
   return (
     <li>
             <span className="player">
-           { playerName}
+           { playerNameTag}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleClick}>{btnCaption}</button>
