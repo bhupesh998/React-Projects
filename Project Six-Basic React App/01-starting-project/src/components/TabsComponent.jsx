8db78bb47815c528a39da53cb64a3a5963fc8558
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-06 22:08:41 +0530

    default props

--- Project Six-Basic React App/01-starting-project/src/components/TabsComponent.jsx
+++ Project Six-Basic React App/01-starting-project/src/components/TabsComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TabsComponent = ({children, buttons, buttonsContainer}) => {
+const TabsComponent = ({children, buttons, buttonsContainer='div'}) => {
 
     const ButtonsContainer = buttonsContainer
     // we can also accept buttonsContainer as ButtonsContainer to avoid assiging and then using , for that the place where its passed we need to pass it as ButtonsContainer="menu"
