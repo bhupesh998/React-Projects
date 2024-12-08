3efe37e8cfade1b7bff072b265b2fffd4a941ca0
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-08 19:03:13 +0530

    Context.consumer approacch

--- Project Eleven -Prop Drilling and Context Api and useReducer/01-starting-project/src/components/Cart.jsx
+++ Project Eleven -Prop Drilling and Context Api and useReducer/01-starting-project/src/components/Cart.jsx
@@ -7,45 +7,55 @@ import { CartContext } from "../store/shopping-cart-context";
 
 
 
-export default function Cart({  onUpdateItemQuantity }) {
-  const cartCtx = useContext(CartContext)
-  const totalPrice = cartCtx.items.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0
-  );
-  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+export default function Cart({ onUpdateItemQuantity }) {
+  // const cartCtx = useContext(CartContext)
+
 
   return (
-    <div id="cart">
-      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
-      {cartCtx.items.length > 0 && (
-        <ul id="cart-items">
-          {cartCtx.items.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
+    <CartContext.Consumer>
+      {(cartConsumerCtx) => {
 
-            return (
-              <li key={item.id}>
-                <div>
-                  <span>{item.name}</span>
-                  <span> ({formattedPrice})</span>
-                </div>
-                <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
-                    +
-                  </button>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p>
-    </div>
+        const totalPrice = cartConsumerCtx.items.reduce(
+          (acc, item) => acc + item.price * item.quantity,
+          0
+        );
+        const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+
+        return (
+          <div id="cart">
+            {cartConsumerCtx.items.length === 0 && <p>No items in cart!</p>}
+            {cartConsumerCtx.items.length > 0 && (
+              <ul id="cart-items">
+                {cartConsumerCtx.items.map((item) => {
+                  const formattedPrice = `$${item.price.toFixed(2)}`;
+
+                  return (
+                    <li key={item.id}>
+                      <div>
+                        <span>{item.name}</span>
+                        <span> ({formattedPrice})</span>
+                      </div>
+                      <div className="cart-item-actions">
+                        <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                          -
+                        </button>
+                        <span>{item.quantity}</span>
+                        <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                          +
+                        </button>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            <p id="cart-total-price">
+              Cart Total: <strong>{formattedTotalPrice}</strong>
+            </p>
+          </div>
+        )
+      }}
+
+    </CartContext.Consumer>
   );
 }
