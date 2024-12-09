ef918568a3dd16b271396ff5a12cd345135e3b15
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-09 21:10:09 +0530

    side effect in app and why statte cannot be used for setting available places

---
+++ Project Twelve-useEffect/01-starting-project/src/App.jsx
@@ -0,0 +1,86 @@
+import { useRef, useState, useEffect } from 'react';
+
+import Places from './components/Places.jsx';
+import { AVAILABLE_PLACES } from './data.js';
+import Modal from './components/Modal.jsx';
+import DeleteConfirmation from './components/DeleteConfirmation.jsx';
+import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js'
+
+function App() {
+  const modal = useRef();
+  const selectedPlace = useRef();
+ // const [availablePlaces, setAvailablePlaces ] = setAvailablePlaces([])
+  const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  
+
+  //below is a side effect code , needed but not necessary
+  navigator.geolocation.getCurrentPosition((position)=>{
+    const sortedPlaces = sortPlacesByDistance(AVAILABLE_PLACES,position.coords.latitude, position.coords.longitude );
+
+   // setAvailablePlaces(sortedPlaces)  it will cause infinite loop as on setting places here component will rerender and on rerender the navigator code will again be called and same cycle of sorting and setting places repeat
+
+  })
+
+  function handleStartRemovePlace(id) {
+    modal.current.open();
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    modal.current.close();
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+  }
+
+  function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    modal.current.close();
+  }
+
+  return (
+    <>
+      <Modal ref={modal}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={'Select the places you would like to visit below.'}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          places={AVAILABLE_PLACES}
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
