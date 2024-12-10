23b83b25e7a3e4ed9183f16448321a56d5bf1297
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-10 21:09:56 +0530

    use of memo

---
+++ Project Thirteen - React Optimization/01-starting-project/src/components/Counter/Counter.jsx
@@ -0,0 +1,74 @@
+import { useState, memo} from 'react';
+
+import IconButton from '../UI/IconButton.jsx';
+import MinusIcon from '../UI/Icons/MinusIcon.jsx';
+import PlusIcon from '../UI/Icons/PlusIcon.jsx';
+import CounterOutput from './CounterOutput.jsx';
+import { log } from '../../log.js';
+
+function isPrime(number) {
+  log(
+    'Calculating if is prime number',
+    2,
+    'other'
+  );
+  if (number <= 1) {
+    return false;
+  }
+
+  const limit = Math.sqrt(number);
+
+  for (let i = 2; i <= limit; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// memo will take a look at props and when the component function executes again for rg when app component executes memo will compare the old and new prop value
+// if the values are exactly same then this function execution will be prevented by memo
+// so this component will execute only if initialCount prop changes or any state inside component changes , memo only prevent function execution that are triggered by parent component
+//  if counter doesn't executes again then its internal components that are used with in it will not be reexecuted
+
+// Dont overuse memo 
+// try to use it higher up in component tree - blocking a component execution at top will also block its child componet execution
+// Dont wrap all your componets with memo as it adds unneccessary overhead of checking props that costs performance, don't use it on components where props changes frequently
+
+const Counter = memo(function Counter({ initialCount }) {
+  log('<Counter /> rendered', 1);
+  const initialCountIsPrime = isPrime(initialCount);
+
+  const [counter, setCounter] = useState(initialCount);
+
+  function handleDecrement() {
+    setCounter((prevCounter) => prevCounter - 1);
+  }
+
+  function handleIncrement() {
+    setCounter((prevCounter) => prevCounter + 1);
+  }
+
+  return (
+    <section className="counter">
+      <p className="counter-info">
+        The initial counter value was <strong>{initialCount}</strong>. It{' '}
+        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+      </p>
+      <p>
+        <IconButton icon={MinusIcon} onClick={handleDecrement}>
+          Decrement
+        </IconButton>
+        <CounterOutput value={counter} />
+        <IconButton icon={PlusIcon} onClick={handleIncrement}>
+          Increment
+        </IconButton>
+      </p>
+    </section>
+  );
+});
+
+
+
+export default Counter
