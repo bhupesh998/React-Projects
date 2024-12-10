2a526a67148d1826e7f61274d68a9fa369f00112
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-10 22:07:07 +0530

    state scheduling and batching

--- Project Thirteen - React Optimization/01-starting-project/src/App.jsx
+++ Project Thirteen - React Optimization/01-starting-project/src/App.jsx
@@ -12,8 +12,13 @@ function App() {
   
   const [chosenCount, setChosenCount] = useState(0);
 
+  // also below we are updating state twice so will component render two times - no
+  // because react performs state baching also - multiple state updates that are triggered from same function are batched together and will only lead to one function execution
   function handleSetCount(newCount){
     setChosenCount(newCount)
+    setChosenCount((prev)=>prev+1 ) // this way gurantees that only the lastest value of state is used for execution , so value that we get on state update at line 16 , will be avialble to use on 17 but bot 18
+    console.log("newCount", newCount); // this will stil show old result as eventough we are updating state in previous line , the state gets updated when component rerenders only
+    
   }
 
   return (
