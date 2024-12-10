e12cd132768f045821187d3302c14a01355d0f3f
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-10 21:30:58 +0530

    useCallback with memo

--- Project Thirteen - React Optimization/01-starting-project/src/components/UI/IconButton.jsx
+++ Project Thirteen - React Optimization/01-starting-project/src/components/UI/IconButton.jsx
@@ -1,6 +1,11 @@
+import { memo } from 'react';
 import { log } from '../../log.js';
 
-export default function IconButton({ children, icon, ...props }) {
+
+// in the counter componet when we are incrementing or decrementing a count , the Iconbutton component was getting rerender on evry change but we can avoid it through memo as its only outputiing buttons that are not changing 
+// but strangely we see that they still gets rendered because a function onclick in getting passed to it and we know that no two function object are same even with same code as they are created on every re-render and the refrences they have is diffrent each time
+// so useCallback for them first and then memo will do its work
+ const IconButton = memo( function IconButton({ children, icon, ...props }) {
   log('<IconButton /> rendered', 2);
 
   const Icon = icon;
@@ -10,4 +15,6 @@ export default function IconButton({ children, icon, ...props }) {
       <span className="button-text">{children}</span>
     </button>
   );
-}
+});
+
+export default IconButton
