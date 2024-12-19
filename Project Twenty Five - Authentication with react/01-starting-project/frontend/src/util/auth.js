ba68eb226136beb0e53157ceb0ccb14cf0eaeccc
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-19 20:45:04 +0530

    restricting access to routes using a loader

--- Project Twenty Five - Authentication with react/01-starting-project/frontend/src/util/auth.js
+++ Project Twenty Five - Authentication with react/01-starting-project/frontend/src/util/auth.js
@@ -1,3 +1,5 @@
+import { redirect } from "react-router-dom"
+
 export function getAuthToken(){
     const token = localStorage.getItem('token')
     return token
@@ -5,4 +7,17 @@ export function getAuthToken(){
 
 export function tokenLoader(){
     return getAuthToken()
+}
+
+// will be used for checking or restrict aaccess to certain routes if we try to manipulate url directly instead of app
+export function checkAuthLoader(){
+
+    const token = getAuthToken()
+
+    if(!token){
+        redirect('/auth')
+    }
+
+    // loader must always return something
+    return null
 }
