e31e3b0ad9f589819e1d0d1dd1249da405c0d3d3
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-19 21:02:26 +0530

    token expiration for useEffect

--- Project Twenty Five - Authentication with react/01-starting-project/frontend/src/pages/Root.js
+++ Project Twenty Five - Authentication with react/01-starting-project/frontend/src/pages/Root.js
@@ -1,9 +1,36 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
   // const navigation = useNavigation();
+
+  const token = useLoaderData()  // will call token loader on root page and its defined on root only so no need to use userouteloader
+  const submit = useSubmit()
+
+  // the flaw in this process is if we reload the page then all components will re render and token expiration timer will start again from 0
+  useEffect(()=>{
+
+    if(!token){
+      return ;
+    }
+
+    if(token === 'EXPIRED'){
+      submit(null , { action: '/logout', method: "POST"})
+      return ;
+    }
+
+    const duration = getTokenDuration()
+    console.log("token duration", duration);
+    
+
+    setTimeout(()=>{
+      submit(null , { action: '/logout', method: "POST"})
+    }, duration)
+
+  }, [token ,submit])
 
   return (
     <>
--- Project Twenty Five - Authentication with react/01-starting-project/frontend/src/util/auth.js
+++ Project Twenty Five - Authentication with react/01-starting-project/frontend/src/util/auth.js
@@ -1,7 +1,24 @@
 import { redirect } from "react-router-dom"
 
+export function getTokenDuration(){
+    const storedExpirationDate =  localStorage.getItem('tokenExp')
+    const expirationDate = new Date(storedExpirationDate)
+    const now = new Date()
+    const duration = expirationDate.getTime() - now.getTime()
+    return duration
+}
+
 export function getAuthToken(){
     const token = localStorage.getItem('token')
+    const duration = getTokenDuration()
+
+    if(!token){
+        return;
+    }
+
+    if(duration < 0){
+        return 'EXPIRED'
+    }
     return token
 }
 
