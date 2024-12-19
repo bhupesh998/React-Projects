93b2a76eb4e1e829f006ca7efcf172fe4454d3f5
Bhupesh <BhupeshChoudhary998@gmail.com>
2024-12-19 12:14:37 +0530

    working app for login and user creation

--- Project Twenty Five - Authentication with react/01-starting-project/frontend/src/pages/Authentication.js
+++ Project Twenty Five - Authentication with react/01-starting-project/frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,40 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export const action = async ({ request, params})=> {
+
+  // constructor given by browser
+  const searchParams = new URL(request.url).searchParams()
+  const mode = searchParams.get('mode') || 'login'
+
+  if(mode !=="login" && mode !== "signup"){
+    throw new Error("mode not supported")
+  }
+
+  const data = await request.formData()
+  const authData = {
+    email : data.get('email'),
+    password: data.get('password')
+  }
+
+  const response = fetch('http://localhost:8080/'+ mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(authData)
+  })
+
+  if(response.status === 422 || response.status === 401){
+    return response
+  }
+
+
+  if(!response.ok){
+    throw new Response({message : "Could not authenticate user"}, {status: 500})
+  }
+
+  redirect('/')
+
+}
