33e2c2843cd6032a0ff9322ae70a4f84f6eb8a87
bhupesh998 <bhupeshchoudhary998@gmail.com>
2024-12-21 15:57:16 +0530

    basic react app with typescript

---
+++ Project Twenty Eight - React with TypeScript/react-with-ts/src/components/Todos.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todos = () => {
+  return <ul>
+    <li>Learn React</li>
+    <li>Learn Typescript</li>
+    <li>Learn Node</li>
+  </ul>
+}
+
+export default Todos
