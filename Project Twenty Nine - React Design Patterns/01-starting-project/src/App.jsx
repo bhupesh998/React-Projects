fe93c275304fc5f174265e80aef508cc86a93e27
bhupesh998 <bhupeshchoudhary998@gmail.com>
2024-12-22 13:10:23 +0530

    debouncing

--- Project Twenty Nine - React Design Patterns/01-starting-project/src/App.jsx
+++ Project Twenty Nine - React Design Patterns/01-starting-project/src/App.jsx
@@ -35,8 +35,11 @@ function App() {
       </Accordian>
     </section>
     <section>
-      <SearchableList items={PLACES} >
+      <SearchableList items={PLACES} itemKeyFn={(item)=> item.id} >
         { (item)=> <Place item={item}/> }
+      </SearchableList>
+      <SearchableList items={["item1" , "item2"]} itemKeyFn={(item)=> item}>
+      { (item)=> item }
       </SearchableList>
     </section>
   </main>
--- Project Twenty Nine - React Design Patterns/01-starting-project/src/components/SearchableList/SearchableList.jsx
+++ Project Twenty Nine - React Design Patterns/01-starting-project/src/components/SearchableList/SearchableList.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
-const SearchableList = ({items , children}) => {
+const SearchableList = ({items , itemKeyFn, children}) => {
 
     const [ serachTerm , setSearchTerm] = useState('')
+    const lastChange = useRef()
 
     const searchResults = items.filter(item => JSON.stringify(item).toLowerCase().includes(serachTerm.toLowerCase()))
 
     function handleChange(event){
-        setSearchTerm(event.target.value)
+        // debouncing logic , but it will still update state for every keystrong just delay by specified seconds
+        setTimeout(()=>{
+            setSearchTerm(event.target.value)
+        }, 2000)
+       
     }
 
   return (
     <div className='searchable-list'>
         <input type="search" placeholder="search" onChange={handleChange} />
         <ul>
-            {searchResults.map(item=><li key={item.id}>{children(item)}</li>)}
+            { /* itemKeyFn for dynamically generate key , as not all items may have a if when we erre using item.id*/}
+            {searchResults.map(item=><li key={itemKeyFn(item)}>{children(item)}</li>)}
         </ul>
     </div>
   )
